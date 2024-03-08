ffebc59231f46ad469e6aa4460d27ca4fb604aae
Rickard Lindahl <rickard@lindahl.app>
2024-03-08 23:09:01 +0100

    fix(cmd-api): Use axum::extract to simplify route

--- apps/cmd-api/src/routes/teams.rs
+++ apps/cmd-api/src/routes/teams.rs
@@ -1,55 +1,13 @@
-use axum::{extract::rejection::JsonRejection, http::StatusCode, Json};
+use axum::{extract, http::StatusCode, Json};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-pub async fn add_team(payload: Result<Json<Team>, JsonRejection>) -> (StatusCode, Json<Response>) {
-    match payload {
-        Ok(_payload) => {
-            let response = IdResponse { id: Uuid::new_v4() };
+pub async fn add_team(
+    extract::Json(_payload): extract::Json<Team>,
+) -> (StatusCode, Json<Response>) {
+    let response = IdResponse { id: Uuid::new_v4() };
 
-            (StatusCode::CREATED, Json(Response::Success(response)))
-        }
-        Err(JsonRejection::MissingJsonContentType(_)) => {
-            let message = MessageResponse {
-                message: "Missing Content-Type: application/json header".into(),
-            };
-
-            (StatusCode::BAD_REQUEST, Json(Response::Error(message)))
-        }
-        Err(JsonRejection::JsonDataError(_)) => {
-            let message = MessageResponse {
-                message: "Couldn't deserialize the body into the target type".into(),
-            };
-
-            (StatusCode::BAD_REQUEST, Json(Response::Error(message)))
-        }
-        Err(JsonRejection::JsonSyntaxError(_)) => {
-            // Syntax error in the body
-            let message = MessageResponse {
-                message: "Syntax error in the body".into(),
-            };
-
-            (StatusCode::BAD_REQUEST, Json(Response::Error(message)))
-        }
-        Err(JsonRejection::BytesRejection(_)) => {
-            // Failed to extract the request body
-            let message = MessageResponse {
-                message: "Failed to extract the request body".into(),
-            };
-
-            (StatusCode::BAD_REQUEST, Json(Response::Error(message)))
-        }
-        Err(_) => {
-            // `JsonRejection` is marked `#[non_exhaustive]` so match must include a catch-all case.
-            let message = MessageResponse {
-                message: "Unknown error".into(),
-            };
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(Response::Error(message)),
-            )
-        }
-    }
+    (StatusCode::CREATED, Json(Response::Success(response)))
 }
 
 #[derive(Deserialize)]
