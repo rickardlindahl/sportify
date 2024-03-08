1a696661175452a8b6ec351e6c2d1435c95553c8
Rickard Lindahl <rickard@lindahl.app>
2024-03-08 23:02:24 +0100

    feat(cmd-api): Add POST /api/teams route
    Kind of a naive implementation with pattern matching
    for the json body etc. Just to try it out.

--- apps/cmd-api/src/main.rs
+++ apps/cmd-api/src/main.rs
@@ -1,20 +1,13 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::post, Router};
+
+mod routes;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root));
+    let app = Router::new().route("/api/teams", post(routes::teams::add_team));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
